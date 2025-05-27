0f1c408851596cc0b23e7a471d700e554797ef1c
dahn <ddahnn@gmail.com>
2025-05-26 21:48:26 -0300

    Lista encadeada

---
+++ dados/main.py
@@ -0,0 +1,18 @@
+from No import No
+from ListaEncadeada import ListaEncadeada
+
+
+lista = ListaEncadeada()
+
+lista.add_No_Final("Daniel")
+lista.add_No_Inicio("Fernanda")
+lista.add_No_Inicio("Pedro")
+lista.add_No_Inicio("Joaquim")
+lista.add_No_Inicio("Jocelino antovaldo")
+
+
+lista.remover_Do_Inicio()
+lista.remover_Do_Fim()
+
+
+lista.imprimnirLista()
